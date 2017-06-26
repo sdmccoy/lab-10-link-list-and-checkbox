5af7b6a82d27035bc5812dcbbc94b01abc29c867
Scott <sdmccoy@gmail.com>
2017-06-25 19:05:00 -0700

    three tests for the find middle method are passing

--- lab10-scott/test/link-list-test.js
+++ lab10-scott/test/link-list-test.js
@@ -1,14 +1,31 @@
 'use strict';
 
-const LinkList = require('./link-list.js');
+const LinkList = require('../link-list.js');
 const expect = require('expect');
 
 describe('Testing LINK LIST', () => {
   describe('Testing the constructor LinkList', () => {
-    it('it should create a new node', () => {
-      let node = new LinkList(1);
-      expect(node.value).toEqual(1);
-      expect(node.next).toEqual(null);
+    describe('If passing in a node and successful', () => {
+      it('it should create a new node', () => {
+        let node = new LinkList(1);
+        expect(node.value).toEqual(1);
+        expect(node.next).toEqual(null);
+      });
+    });
+    describe('If passing in no node', () => {
+      it('it should not create a new node', () => {
+        let node = new LinkList();
+        expect(node.value).toNotExist();
+        expect(node.next).toNotExist();
+      });
+    });
+    describe('If passing in a value that is not a node', () => {
+      it('it should not create a new node', () => {
+        let notanode;
+        let node = new LinkList(notanode);
+        expect(node.value).toNotExist();
+        expect(node.next).toNotExist();
+      });
     });
   });
   describe('Testing the appendNode method', () => {
@@ -16,7 +33,7 @@ describe('Testing LINK LIST', () => {
       it('it should append a new node to the end of the chain', () =>{
         let node = new LinkList(1);
         let secondNode = new LinkList(2);
-        LinkList.appendNode(secondNode);
+        node.appendNode(secondNode);
         expect(secondNode.value).toEqual(2);
         expect(secondNode.next).toEqual(null);
         expect(node.next).toEqual(secondNode);
@@ -25,19 +42,52 @@ describe('Testing LINK LIST', () => {
     describe('if passing through a no value/node', ()=>{
       it('it should not append a new node', () =>{
         let node = new LinkList(1);
-        let secondNode = new LinkList();
-        LinkList.appendNode(secondNode);
-        expect(secondNode).toNotExist();
+        node.appendNode();
+        expect(node.value).toEqual(1);
         expect(node.next).toEqual(null);
       });
     });
     describe('if passing through a non node value', () => {
       it('it should not append a new node', () =>{
         let node = new LinkList(1);
-        let secondNode = new LinkList('notanode');
-        LinkList.appendNode(secondNode);
-        expect(secondNode).toNotExist();
+        node.appendNode('notanode');
         expect(node.next).toEqual(null);
+      });
+    });
+  });
+  describe('Testing the findMiddle method', () => {
+    describe('if successful', () =>{
+      it('it should find the middle to be 2', () =>{
+        let node = new LinkList(1);
+        let secondNode = new LinkList(2);
+        let thirdNode = new LinkList(3);
+        node.appendNode(secondNode);
+        node.appendNode(thirdNode);
+        let middleNode = node.findMiddle();
+        expect(middleNode.value).toEqual(2);
+        expect(middleNode.next).toEqual(thirdNode);
+      });
+    });
+    describe('if passing through an even amount of nodes', ()=>{
+      it('it should return the node on the left side', () =>{
+        let node = new LinkList(1);
+        let secondNode = new LinkList(2);
+        let thirdNode = new LinkList(3);
+        let fourthNode = new LinkList(4);
+        node.appendNode(secondNode);
+        node.appendNode(thirdNode);
+        node.appendNode(fourthNode);
+        let middleNode = node.findMiddle();
+        expect(middleNode.value).toEqual(2);
+        expect(middleNode.next).toEqual(thirdNode);
+      });
+    });
+    describe('if passing through just one node', () => {
+      it('it should still find just the one node', () =>{
+        let node = new LinkList(1);
+        let middleNode = node.findMiddle();
+        expect(middleNode.value).toEqual(1);
+        expect(middleNode.next).toEqual(null);
       });
     });
   });
